05486facbb8c2ba309b17efdd24c7882d3266440
Hichoow <h.elfaquir52@gmail.com>
2021-10-12 20:11:46 +0200

    Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module nl.hsleiden.iipsene2applicatie {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens nl.hsleiden.iipsene2applicatie to javafx.fxml;
+    exports nl.hsleiden.iipsene2applicatie;
+    exports nl.hsleiden.controller;
+    opens nl.hsleiden.controller to javafx.fxml;
+}
