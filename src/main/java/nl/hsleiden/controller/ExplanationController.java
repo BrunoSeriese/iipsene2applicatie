bcc512408be90ded0dd4cf097e221dba44b028f2
Bruno <bruno.pleysier@gmail.com>
2021-10-13 14:03:26 +0200

    explanationView werkt nu samen met zijn controller

--- src/main/java/nl/hsleiden/controller/ExplanationController.java
+++ src/main/java/nl/hsleiden/controller/ExplanationController.java
@@ -7,17 +7,23 @@ public class ExplanationController {
     private static ExplanationController explanationController;
     private final ExplanationDAO explanationDAO;
     private final ExplanationService explanationService;
-
+    private final SceneController sceneController;
     public ExplanationController(ExplanationDAO explanationDAO,
                                  ExplanationService explanationService) {
         this.explanationDAO = explanationDAO;
         this.explanationService = explanationService;
+        sceneController = SceneController.getInstance();
     }
+
+
 
     public static ExplanationController getInstance() {
         if(explanationController == null) {
             explanationController = new ExplanationController(new ExplanationDAO(), new ExplanationService());
         }
         return explanationController;
+    }
+    public void sendNextContent(){
+    System.out.println("nu krijg je meer te zien");
     }
 }
--- src/main/java/nl/hsleiden/view/ExplanationView.java
+++ src/main/java/nl/hsleiden/view/ExplanationView.java
@@ -1,14 +1,22 @@
 package nl.hsleiden.view;
 
+import nl.hsleiden.controller.ExplanationController;
+import nl.hsleiden.controller.MainController;
 import nl.hsleiden.observer.ExplanationObserver;
 import nl.hsleiden.subject.ExplanationSubject;
 
 public class ExplanationView implements ExplanationObserver {
+
+    private final ExplanationController explanationController;
+    public ExplanationView(){
+        explanationController = ExplanationController.getInstance();
+    }
     @Override
     public void update(ExplanationSubject state) {
 
     }
     public void nextContent(){
         System.out.println("next content is coming!");
+        explanationController.sendNextContent();
     }
 }
