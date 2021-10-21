d8eebc9136d155bad3066c1997d01285c7ce9e3c
VinSe <vincentmcmaps@gmail.com>
2021-10-21 15:08:59 +0200

    Added getAll method

--- src/main/java/nl/hsleiden/DAO/ExplanationDAO.java
+++ src/main/java/nl/hsleiden/DAO/ExplanationDAO.java
@@ -1,7 +1,15 @@
 package nl.hsleiden.DAO;
 
+import nl.hsleiden.model.Answer;
 import nl.hsleiden.model.Explanation;
+import nl.hsleiden.model.Video;
+import org.apache.commons.io.IOUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 public class ExplanationDAO implements DAO<Explanation> {
@@ -33,5 +41,25 @@ public class ExplanationDAO implements DAO<Explanation> {
     @Override
     public void delete(Explanation explanation) {
 
+    }
+
+    public void getExplanations() throws IOException {
+        JSONArray json = new JSONArray(IOUtils.toString(new URL("http://localhost:8080/explanations"), StandardCharsets.UTF_8));
+
+        for (int i = 0; i < json.length(); i++) {
+            JSONObject explanations = (JSONObject) json.get(i);
+            int eid = (int) explanations.get("id");
+            String evalue = (String) explanations.get("value");
+
+            JSONObject answer = (JSONObject) explanations.get("answer");
+            int aid = (int) answer.get("id");
+            String avalue = (String) answer.get("value");
+            int acurrentContentId = (int) answer.get("currentContentId");
+            int anextContentId = (int) answer.get("nextContentId");
+
+            Answer newAnswer = new Answer(aid, avalue, acurrentContentId, anextContentId);
+
+            Explanation newExplanation = new Explanation(eid, evalue, newAnswer);
+        }
     }
 }
