c24997d95ac18d7dbfd9f45d695d4dd14c27ae44
VinSe <vincentmcmaps@gmail.com>
2021-11-04 15:18:51 +0100

    Optimized/Improved DAO

--- src/main/java/nl/hsleiden/DAO/ExplanationDAO.java
+++ src/main/java/nl/hsleiden/DAO/ExplanationDAO.java
@@ -1,54 +1,32 @@
 package nl.hsleiden.DAO;
 
-import nl.hsleiden.model.Answer;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import nl.hsleiden.model.Explanation;
-import nl.hsleiden.service.AnswerService;
 import nl.hsleiden.service.ApiService;
-import nl.hsleiden.service.ExplanationService;
 import org.apache.commons.io.IOUtils;
 import org.json.JSONArray;
-import org.json.JSONObject;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ExplanationDAO implements DAO<Explanation> {
-    private final String api = new ApiService().getApi();
-    private final AnswerService answerService;
-    private final ExplanationService explanationService;
-
-    public ExplanationDAO(ExplanationService explanationService) {
-        answerService = new AnswerService();
-        this.explanationService = explanationService;
-    }
+    private final String api = new ApiService().getApi("/explanations");
 
     @Override
     public List<Explanation> getAll() {
         List<Explanation> explanations = new ArrayList<>();
         try {
-            JSONArray json = new JSONArray(IOUtils.toString(new URL(api + "/explanations"), StandardCharsets.UTF_8));
-
-            for (int i = 0; i < json.length(); i++) {
-                JSONObject explanation = (JSONObject) json.get(i);
-                Explanation newExplanation = explanationService.unpack(explanation);
-
-                JSONObject answer = (JSONObject) explanation.get("answer");
-                Answer newAnswer = answerService.unpack(answer);
-                newExplanation.setAnswer(newAnswer);
-
-                explanations.add(newExplanation);
-            }
+            JSONArray json = new JSONArray(IOUtils.toString(new URL(api), StandardCharsets.UTF_8));
+            Type type = new TypeToken<ArrayList<Explanation>>(){}.getType();
+            explanations = new Gson().fromJson(json.toString(), type);
         } catch(IOException e) {
             e.printStackTrace();
         }
         return explanations;
-    }
-
-    @Override
-    public Explanation get(Long id) {
-        return null;
     }
 }
--- src/main/java/nl/hsleiden/DAO/ResultDAO.java
+++ src/main/java/nl/hsleiden/DAO/ResultDAO.java
@@ -1,54 +1,32 @@
 package nl.hsleiden.DAO;
 
-import nl.hsleiden.model.Answer;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import nl.hsleiden.model.Result;
-import nl.hsleiden.service.AnswerService;
 import nl.hsleiden.service.ApiService;
-import nl.hsleiden.service.ResultService;
 import org.apache.commons.io.IOUtils;
 import org.json.JSONArray;
-import org.json.JSONObject;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ResultDAO implements DAO<Result> {
-    private final String api = new ApiService().getApi();
-    private final AnswerService answerService;
-    private final ResultService resultService;
-
-    public ResultDAO(ResultService resultService) {
-        answerService = new AnswerService();
-        this.resultService = resultService;
-    }
+    private final String api = new ApiService().getApi("/results");
 
     @Override
     public List<Result> getAll() {
         List<Result> results = new ArrayList<>();
         try {
-            JSONArray json = new JSONArray(IOUtils.toString(new URL(api + "/results"), StandardCharsets.UTF_8));
-
-            for (int i = 0; i < json.length(); i++) {
-                JSONObject result = (JSONObject) json.get(i);
-                Result newResult = resultService.unpack(result);
-
-                JSONObject answer = (JSONObject) result.get("answer");
-                Answer newAnswer = answerService.unpack(answer);
-                newResult.setAnswer(newAnswer);
-
-                results.add(newResult);
-            }
-        } catch(IOException e) {
+            JSONArray json = new JSONArray(IOUtils.toString(new URL(api), StandardCharsets.UTF_8));
+            Type type = new TypeToken<ArrayList<Result>>(){}.getType();
+            results = new Gson().fromJson(json.toString(), type);
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return results;
-    }
-
-    @Override
-    public Result get(Long id) {
-        return null;
     }
 }
